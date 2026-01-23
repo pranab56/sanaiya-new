e487803009ed0f94bae16626ab9fcd12fbddbe50
pranab56 <pronabhalder53@gmail.com>
2026-01-23 15:18:15 +0600

    mend

--- app/invoice/[id]/page.tsx
+++ app/invoice/[id]/page.tsx
@@ -269,7 +269,9 @@ const Page = () => {
 
                           <div className="flex-1 border-r-3 border-gray-600 px-6 py-6 flex items-center justify-center print:px-6 print:py-6">
                             <span className="text-md font-bold tracking-wide print:text-md">
-                              {invoiceData?.car?.plateNumberForSaudi?.alphabetsCombinations?.[1] || 'N/A'}
+                              {invoiceData?.car?.plateNumberForSaudi?.alphabetsCombinations?.[1]
+                                ?.split('')
+                                .join(' ') || 'N/A'}
                             </span>
                           </div>
                         </div>
@@ -281,7 +283,9 @@ const Page = () => {
                           </div>
                           <div className="flex-1 border-r-3 border-gray-600 px-6 py-6 flex items-center justify-center print:px-6 ">
                             <span className="text-md font-bold print:text-md" style={{ fontFamily: 'Arial' }}>
-                              {invoiceData?.car?.plateNumberForSaudi?.alphabetsCombinations?.[0] || 'N/A'}
+                              {invoiceData?.car?.plateNumberForSaudi?.alphabetsCombinations?.[0]
+                                ?.split('')
+                                .join(' ') || 'N/A'}
                             </span>
                           </div>
                         </div>
